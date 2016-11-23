579f4c1924fca7b30dc9a12c084dbadbda3b41f5
Scott Kaye <pgreviews@gmail.com>
2016-11-23 14:10:19 -0500

    Add project files.

---
+++ PictureTagger/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PictureTagger.MvcApplication" Language="C#" %>
---
+++ PictureTagger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PictureTagger.Startup))]
+namespace PictureTagger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
