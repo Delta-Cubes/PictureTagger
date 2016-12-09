914337ab554dd438dcadcc1a2aac2c176810727f
Scott Kaye <pgreviews@gmail.com>
2016-12-08 19:47:20 -0500

    Big money UI updates

--- PictureTagger/App_Start/BundleConfig.cs
+++ PictureTagger/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace PictureTagger
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+					  "~/Content/site.css",
+					  "~/Content/PictureTagger.min.css"));
 		}
 	}
 }
