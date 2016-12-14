05c73dbe4dffb7bb421c700d7aea3cd0159c4734
Scott Kaye <pgreviews@gmail.com>
2016-12-13 20:31:44 -0500

    Use object as keys

--- PictureTagger/Repositories/IRepository.cs
+++ PictureTagger/Repositories/IRepository.cs
@@ -9,10 +9,10 @@ namespace PictureTagger.Repositories
 	public interface IRepository<T> : IDisposable
 	{
 		IQueryable<T> GetAll();
-		T Find(int? id);
+		T Find(params object[] keyValues);
 		void Create(T model);
 		void Update(T model);
-		void Delete(int? id);
+		void Delete(params object[] keyValues);
 		void Delete(T model);
 	}
 }
