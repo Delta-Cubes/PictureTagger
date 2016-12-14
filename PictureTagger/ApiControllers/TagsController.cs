94090121eb5ff4e43cd9f945a7ecfc2bcd8d383b
Blake Randall <blake.randall.oakville@gmail.com>
2016-12-13 21:15:56 -0500

    Removed Api Controller for Pictures, as it is uneeded. Removed CRUD Operations from Api Controller for Tags, as we only need a auto complete route.

--- PictureTagger/ApiControllers/TagsController.cs
+++ PictureTagger/ApiControllers/TagsController.cs
@@ -24,94 +24,11 @@ namespace PictureTagger.ApiControllers
             this._db = db;
         }
 
-        // GET: api/Tags
-        [AllowAnonymous]
-        public IQueryable<TagApi> GetTags()
-        {
-            return _db.GetAll().RealCast<TagApi>();
-        }
-
-        // GET: api/Tags/5
-        [AllowAnonymous]
-        [ResponseType(typeof(TagApi))]
-        public IHttpActionResult GetTag(int id)
-        {
-            Tag tag = _db.Find(id);
-            if (tag == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(tag);
-        }
-
         [AllowAnonymous]
         [Route("api/Tags/Suggestions/{partialTag}")]
         public IQueryable<TagApi> Suggestions(string partialTag)
         {
             return _db.GetAll().Where(t => t.TagLabel.Contains(partialTag)).RealCast<TagApi>();
-        }
-
-        // PUT: api/Tags/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutTag(int id, TagApi tagApi)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != tagApi.TagID)
-            {
-                return BadRequest();
-            }
-
-            try
-            {
-                _db.Update(tagApi);
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TagExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Tags
-        [ResponseType(typeof(Tag))]
-        public IHttpActionResult PostTag(TagApi tagApi)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _db.Create(tagApi);
-
-            return CreatedAtRoute("DefaultApi", new { id = tagApi.TagID }, tagApi);
-        }
-
-        // DELETE: api/Tags/5
-        [ResponseType(typeof(Tag))]
-        public IHttpActionResult DeleteTag(int id)
-        {
-            Tag tag = _db.Find(id);
-            if (tag == null)
-            {
-                return NotFound();
-            }
-
-            _db.Delete(tag);
-
-            return Ok(tag);
         }
 
         protected override void Dispose(bool disposing)
